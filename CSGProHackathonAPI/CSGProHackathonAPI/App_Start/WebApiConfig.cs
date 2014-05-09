716c119b938e136776411ad0e23995988839395d
James Churchill <james@smashdev.com>
2014-05-08 20:58:37 -0700

    Added ForceHttpsAttribute filter attribute

--- CSGProHackathonAPI/CSGProHackathonAPI/App_Start/WebApiConfig.cs
+++ CSGProHackathonAPI/CSGProHackathonAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSGProHackathonAPI.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,6 +11,9 @@ namespace CSGProHackathonAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+#if !DEBUG
+            config.Filters.Add(new ForceHttpsAttribute());
+#endif
 
             // Web API routes
             config.MapHttpAttributeRoutes();
